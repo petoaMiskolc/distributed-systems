95b39113d51855e194effe2b30338136fa517990
Albert Pető <peto5@jerry.iit.uni-miskolc.hu>
2016-12-09 11:19:37 +0100

    Switch to Spring boot

---
+++ MVCNew/src/main/java/hu/meiit/Application.java
@@ -0,0 +1,23 @@
+package hu.meiit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/*
+ * This class is only needed to be able to compile with spring boot 
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+}
+
+}
